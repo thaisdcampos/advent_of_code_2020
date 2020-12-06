3bf33e87c0e3aec6eb3c91e4cae3913a08771bb0
Thais de Campos <thaisd.campos@gmail.com>
2020-12-06 14:18:45 -0300

    Use argumets of number to solve two puzzles parts

--- day_01/puzzle_01.rb
+++ day_01/puzzle_01.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
-file = File.open('input_01.txt')
-numbers = file.readlines.map(&:to_i)
-combination = numbers.combination(2).to_a
+ARGV.each do |combination_number|
+  puts "Number of combination: #{combination_number}"
 
-combination2020 = []
-combination.map do |pair|
-  sum_result = pair.inject(:+)
-  combination2020 << pair if sum_result == 2020
+  file = File.open('input_01.txt')
+  numbers = file.readlines.map(&:to_i)
+  combination = numbers.combination(combination_number.to_i).to_a
+
+  combination2020 = []
+  combination.map do |pair|
+    sum_result = pair.inject(:+)
+    combination2020 << pair if sum_result == 2020
+  end
+
+  puts "Combine = #{combination2020}"
+
+  result = combination2020.flatten.inject(:*)
+
+  puts "Result = #{result}"
 end
-
-puts "Combine = #{combination2020}"
-
-result = combination2020.flatten.inject(:*)
-
-puts "Result = #{result}"
