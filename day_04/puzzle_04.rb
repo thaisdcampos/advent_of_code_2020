a2fe4857579328aae29ad3e5185cf5a429e0cd32
Thais de Campos <thaisd.campos@gmail.com>
2020-12-15 22:59:07 -0300

    Final solution

--- day_04/puzzle_04.rb
+++ day_04/puzzle_04.rb
@@ -10,15 +10,16 @@ documents = []
 sum = 0
 (passaports + ['']).map do |passaport|
   if passaport.empty?
-    fields = documents.each { |document| document.split(/\s/) }
+    fields = documents.map { |document| document.split(/\s/) }.flatten
     fields.each do |field|
       key, value = field.split(':')
       passaports_hash[key] = value
     end
 
     remaining_fields = (REQUIRED_FIELDS - passaports_hash.keys)
-    valid = remaining_fields.empty? || remaining_fields.eql?('cid')
+    valid = remaining_fields.empty? || remaining_fields.eql?(['cid'])
     documents.clear
+    passaports_hash.clear
 
     sum += 1 if valid
   else
